64d7c4a66ca7c2ffbdd9fad92d653607a99e6285
javi <javi.munoz.galindo@gmail.com>
2021-08-18 17:10:33 +0100

    feat: update service to use error handling and joi

--- src/services/streams.js
+++ src/services/streams.js
@@ -1,11 +1,33 @@
 const router = require("express").Router();
-router.get("/:userId", getById);
+const  {MAX_NUM_STREAM_CONCURRENTLY} = require("../../config/config");
+const validateUserId = require("../utils/validateUserId");
+const formatErrors = require("../utils/formatErrors");
 const {getNumOfConcurrentStreamByUserId} = require("../database/streamUsers");
+router.get("/:userId", getById);
 
 function getById(req, res) {
-    const numStreams = getNumOfConcurrentStreamByUserId(req.params.userId);
-
-    return res.json({numStreams});
+    try {
+        const numberStreams = validateAndGetNumberOfStreams(req.params);
+        return res.status(200).json({numberStreams});
+    } catch(error) {
+        return res.status(error.code).json({"errorCode":error.code, "message":error.message});
+    }
 }
 
+function validateAndGetNumberOfStreams(params) {
+    const isInvalid = validateUserId(params);
+    if(!isInvalid) {
+        const numStreams = getNumOfConcurrentStreamByUserId(params.userId);
+        if(numStreams) {
+            if(numStreams < MAX_NUM_STREAM_CONCURRENTLY){
+                return numStreams;
+            } else {
+                throw formatErrors("Forbidden", "This user already has 3 concurrent streams");        
+            }
+        } else {
+            throw formatErrors("NotFound", "User not found");
+        }
+    }
+    throw formatErrors("Validation", isInvalid.message);
+}
 module.exports = router;
